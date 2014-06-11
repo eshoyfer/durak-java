a1ee879d0574598cdf7de6c98d618283f0516654
eshoyfer <eshoyfer@gmail.com>
2014-06-10 23:41:02 -0400

    Added Durak.java for testing (main game running thing to be)

---
+++ Durak.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import cardstatic.*;
+
+public class Durak {
+	public static String TRUMP;
+
+	public Durak() {
+		TRUMP = "Hearts";
+	}
+
+	public Durak(String t) {
+		TRUMP = t;
+	}
+}
