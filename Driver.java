cac566a9498a3f1cb455c00799bc8051b22bd957
eshoyfer <eshoyfer@gmail.com>
2014-06-12 18:03:29 -0400

    Driver testing

---
+++ Driver.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import cardstatic.*;
+
+public class Driver {
+	public static void main(String[] args) {
+		Deck d = new Deck();
+		System.out.println(d);
+	}
+}
