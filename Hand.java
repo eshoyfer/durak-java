4f9d23b3e217bc05a2b9559059adff149677080e
eshoyfer <eshoyfer@gmail.com>
2014-06-14 17:36:47 -0400

    Hand class added

---
+++ Hand.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+import cardstatic.*;
+
+public class Hand {
+
+	private ArrayList<Card> cards;
+
+	// Empty hand
+	public Hand() {
+		cards = new ArrayList<Card>();
+	}
+
+	// Hand of n random cards
+	public Hand(int n) {
+		cards = new ArrayList<Card>();
+		for (int i = 0; i < n; i++) {
+			Card thisCard = new Card();
+			cards.add(thisCard);
+		}
+	}
+
+	public void add(Card c) {
+		cards.add(c);
+	}
+
+	public void remove(Card c) {
+		cards.remove(c);
+	}
+
+	public int size() {
+		return cards.size();
+	}
+
+	public boolean needsToDraw() {
+		if (size() > 6) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int numberToDraw() {
+		if (needsToDraw()) {
+			return 0; 
+		} else {
+			return 6 - size();
+		}
+	}
+
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+	@Override
+	public String toString() {
+		String ret = new String();
+		for (Card c : cards) {
+			ret += c + "\n";
+		}
+		return ret;
+	}
+
+}
